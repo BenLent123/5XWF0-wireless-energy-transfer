4a2f5d50f20f1ed0135937ccc82c88b8715b5093
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2024-05-11 00:42:29 +0200

    Update 5XWF0_WET_Algorithm.ino
    added complementary PWM functions

--- 5XWF0_WET_Algorithm.ino
+++ 5XWF0_WET_Algorithm.ino
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include "arduino.h"
 //constants
+  //timer
+unsigned long frequency_of_timers = 1000;
+unsigned long dead_time_timer_1_milisec = 10; //dead time in miliseconds
+unsigned long dead_time_timer_2_milisec = 15; //dead time in miliseconds 
   //pins
     //outputs
 const int pin_dcdc_pwm_output = 1;    // pin for output
@@ -19,7 +23,10 @@ const int channelDCtoDC = 0;
 const int channelDCtoAC = 1;
 const int channelDCtoAC2 = 1;
 const int measurement_resistor = 10000;
-//DCTODC
+//variables
+  //DCTODC
+int DCtoAC_pwm_pin_state = LOW;
+int DCtoAC_pwm_pin_complement_state = HIGH;
 int dutycycle_DC_to_DC = 50;
 float dutycycle_DC_to_DC_prediction = 0;
 float v2 = 0;
@@ -29,9 +36,13 @@ float p1 = 0;
 float v1 = 0; 
 float i1 = 0;
 float vd = 0;
-//DCTOAC
+  //DCTOAC
 int dutycycle_DC_to_AC=0;
 float v_ac = 0;
+  //timers
+unsigned long current_time_timer_1 = 0; 
+unsigned long current_time_timer_2 = 0;   
+
 
 void setup(){
   // set output mode
@@ -55,6 +66,7 @@ void setup(){
 }
 
 void measure() {
+ 
   //DC to DC measurements
   vd = analogRead(pin_dcdc_vi_measurement);
   v2 = analogRead(pin_dcdc_vo_measurement); 
@@ -66,14 +78,12 @@ void measure() {
 }
 
 int DC_to_DC_converter_predicted_output(int d){
-  d =  (20)/(vd-20);
+  d =  (20)/(vd-20); //predict duty cycle required to get 20 volts using equation
   return d;
 }
 
 int MPPT(int d) {
-
   float p2 = v2 * i2;
-
   if (p2 - p1 == 0) 
   {
     d=d+0.1;
@@ -98,27 +108,48 @@ int MPPT(int d) {
     return d;
 }
 
+void DCtoAC_PWM(int freq, int dutycycle){
+  unsigned long frequency_of_timer_1 = freq;
+  time_in_miliseconds = (1/(frequency_of_timer_1))*1000+dead_time_timer_1_milisec;
+ if (millis() - current_time_timer_1 > time_in_miliseconds*dutycycle) //check if time interval has passed 
+  {
+      current_time_timer_1 =  current_time_timer_1 + time_in_miliseconds;
+      //change state
+      if (DCtoAC_pwm_pin_state == LOW)
+      { 
+        DCtoAC_pwm_pin_state = HIGH;
+      }
+      else
+      {
+      DCtoAC_pwm_pin_state = LOW;
+      }
+      // write to the output pin of the DCtoAC pin
+      digitalWrite(pin_dcac_pwm_output, DCtoAC_pwm_pin_state);
+  }
+}
+
+void DCtoAC_PWM_Complement(int freq, int dutycycle){
+  unsigned long frequency_of_timer_2 = freq;
+  time_in_miliseconds = (1/(frequency_of_timer_2))*1000-dead_time_timer_2_milisec; // converts frequency to the time period in miliseconds
+ if (millis() - current_time_timer_2 > (time_in_miliseconds*dutycycle)) //check if time interval has passed where time interval is time period * dutycycle 
+  {
+      current_time_timer_2 =  current_time_timer_2 + time_in_miliseconds;
+      //change state --> state machine 
+      if (DCtoAC_pwm_pin_complement_state == LOW)
+      { 
+        DCtoAC_pwm_pin_complement_state = HIGH;
+      }
+      else
+      {
+      DCtoAC_pwm_pin_complement_state = LOW;
+      }
+      // write to the output pin of the complement DCtoAC pin
+      digitalWrite(pin_dcac_pwm_complement_output, DCtoAC_pwm_pin_complement_state);
+  }
+}
 
 void pwmDCtoDC(int dutycycle) {
   ledcWrite(channelDCtoDC, dutycycle_DC_to_DC);
-}
-
-void pwmDCtoAC(int freq, int dutycycle) {
-  int t_tot = 1/freq;
-  int t_on = t_tot*(1-dutycycle);
-  int t_off = t_tot-t_on;
-  // PWM for output 1
-  ledcWrite(channelDCtoAC, dutycycle_DC_to_AC);
-  //  low for output 2
-  digitalWrite(pin_dcac_pwm_complement_output, LOW);
-  //time on - deadtime
-  delayMicroseconds(t_on-0.1*t_tot); 
-  // high for output 2
-  digitalWrite(pin_dcac_pwm_output, LOW);
-  digitalWrite(pin_dcac_pwm_complement_output, HIGH);
-  //time off - deadtime
-  delayMicroseconds(t_off-0.1*t_tot);
-  
 }
 
 void loop() {
@@ -127,15 +158,14 @@ void loop() {
 
   //calculation functions
     //predict dutycycle using buck-boost equation
-  dutycycle_DC_to_DC_prediction = DC_to_DC_converter_predicted_output(dutycycle_DC_to_DC_prediction);
+    dutycycle_DC_to_DC_prediction = DC_to_DC_converter_predicted_output(dutycycle_DC_to_DC_prediction);
     //change duty cycle using MPPT algorithm
-  dutycycle_DC_to_DC = MPPT(dutycycle_DC_to_DC); 
+    dutycycle_DC_to_DC = MPPT(dutycycle_DC_to_DC); 
     //variable to check difference of duty cycle output of equation vs MPPT --> no actual effect
-  int diff_D = abs((dutycycle_DC_to_DC_prediction / dutycycle_DC_to_DC)*100); 
+    int diff_D = abs((dutycycle_DC_to_DC_prediction / dutycycle_DC_to_DC)*100); 
 
   //pwm functions
   pwmDCtoDC(dutycycle_DC_to_DC);
-  pwmDCtoAC(freq, dutycycle_DC_to_AC);
-
-
+  DCtoAC_PWM(freq, dutycycle_DC_to_AC);
+  DCtoAC_PWM_Complement(freq, dutycycle_DC_to_AC);
 }
