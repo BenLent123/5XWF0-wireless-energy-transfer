b522e3b2ca8b1d76fa5d1fc79365544b652f98fd
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2024-05-10 16:58:27 +0200

    Add files via upload

--- 5XWF0_WET_timer.ino
+++ 5XWF0_WET_timer.ino
@@ -1,7 +1,10 @@
-unsigned long current_time_timer_1 = 0;        // will store last time LED was updated
-unsigned long frequency_of_timer_1 = 1000;
-unsigned long current_time_timer_1 = 0;        // will store last time LED was updated
-unsigned long frequency_of_timer_1 = 2000; // interval at which to blink (milliseconds)
+//constants
+unsigned long frequency_of_timers = 1000;
+unsigned long dead_time_timer_1 = 10;
+unsigned long dead_time_timer_2 = 15;  
+//variables
+unsigned long current_time_timer_1 = 0; 
+unsigned long current_time_timer_2 = 0;      
 
 void setup() 
 {
@@ -10,22 +13,23 @@ void setup()
 
 void loop()
 {
- timer_1_function(frequency_of_timer_1);
+ timer_1_function(frequency_of_timers);
+ timer_2_function(frequency_of_timers);
 }
 
 
-void timer_1_function(unsigned long frequency_of_timer_1 )
+void timer_1_function(unsigned long frequency_of_timer_1)
 {
-  time_in_miliseconds = (1/(frequency_of_timer_1))*1000;
+  time_in_miliseconds = (1/(frequency_of_timer_1))*1000+dead_time_timer_1;
  if (millis() - current_time_timer_1 > time_in_miliseconds) 
   {
       current_time_timer_1 =  current_time_timer_1 + time_in_miliseconds;
   }
 }
 
-void timer_2_function(unsigned long frequency_of_timer_2 )
+void timer_2_function(unsigned long frequency_of_timer_2)
 {
-  time_in_miliseconds = (1/(frequency_of_timer_1))*1000;
+  time_in_miliseconds = (1/(frequency_of_timer_2))*1000-dead_time_timer_2;
  if (millis() - current_time_timer_2 > time_in_miliseconds) 
   {
       current_time_timer_2 =  current_time_timer_2 + time_in_miliseconds;
